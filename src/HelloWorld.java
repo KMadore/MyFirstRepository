433ac2c9dc328f8f37aeac1aba2321962db5579a
karenmadore <karenmadore@aa0508-pc44.labs.sce.carleton.ca>
2012-10-09 08:46:33 -0400

    Added hello method

---
+++ src/HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public String hello(){
+		return "hello";
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
